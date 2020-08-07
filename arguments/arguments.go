b6136bdea2da274c23ceef4819d513e9d99092e4
FletcherCutting <fletcher@fletchercutting.com.au>
2020-08-07 20:38:43 +1000

    [SD-17] Export retrieved data in XML or JSON form

---
+++ arguments/arguments.go
@@ -0,0 +1,38 @@
+package arguments
+
+type Arguments struct {
+    Processes    bool
+    OutputScreen bool
+    OutputXML    bool
+    OutputJSON   bool
+}
+
+func Parse(args []string) Arguments {
+    arguments := newArguments()
+    
+    for _, arg := range args {
+        switch arg {
+        case "-p":
+            arguments.Processes = true
+        case "-oS":
+            arguments.OutputScreen = true
+        case "-oX":
+            arguments.OutputXML = true
+        case "-oJ":
+            arguments.OutputJSON = true
+        }
+    }
+    
+    return arguments
+}
+
+func newArguments() Arguments {
+    var arguments Arguments
+    
+    arguments.Processes    = false
+    arguments.OutputScreen = false
+    arguments.OutputXML    = false
+    arguments.OutputJSON   = false
+    
+    return arguments
+}
